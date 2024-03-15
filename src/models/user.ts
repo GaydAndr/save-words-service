53d2e8148e1ed713aa88902e1895952e22ab370e
andrii_h <galgayand@gmail.com>
2024-03-15 18:18:39 +0200

    Update user.ts

---
+++ src/models/user.ts
@@ -0,0 +1,62 @@
+import { Schema, model } from 'mongoose';
+import  Joi from "joi";
+import {handleMongooseSchemaError} from "../helpers";
+
+interface UserSchema {
+    password: string,
+    email: string,
+    token?: string
+}
+
+interface UserDocument extends UserSchema {
+    _id: string
+}
+
+// Схема користувача
+const userSchema = new Schema<UserDocument>(
+    {
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+        },
+        token: {
+            type: String,
+            default: null,
+        },
+    },
+    { versionKey: false }
+)
+
+userSchema.pre('save',handleMongooseSchemaError as any)
+// handleMongooseSchemaError
+
+// Схеми для валідації
+const registerSchema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().min(8).required(),
+});
+
+const loginSchema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().min(8).required(),
+});
+
+const emailSchema = Joi.object({
+    email: Joi.string().required(),
+});
+
+const schemas = {
+    registerSchema,
+    loginSchema,
+    emailSchema
+};
+
+// Модель користувача
+const User = model<UserDocument>('user', userSchema);
+
+export { schemas, User };
