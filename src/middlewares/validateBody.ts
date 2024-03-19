8efc3bfec0db350fafb580e2d4d9e7a7577da803
andrii_h <galgayand@gmail.com>
2024-03-19 23:06:30 +0200

    Update validateBody.ts

--- src/middlewares/validateBody.ts
+++ src/middlewares/validateBody.ts
@@ -1,12 +1,13 @@
 import {Schema} from 'joi';
-import {NextFunction} from "express";
+import {NextFunction, Request, Response} from "express";
 import {RequestError} from "../helpers";
 
-const validateBody = (schema:Schema)=>{
+const validateBody = (schema: Schema) => {
     return (req: Request, _: Response, next: NextFunction) => {
         const {error} = schema.validate(req.body)
         if (error) {
             next(new RequestError(400, error.message))
+            return
         }
         next()
     }
